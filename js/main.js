a3c88df13ce7a26376a69716560b8610f393442e
Chad Granum <exodist7@gmail.com>
2015-06-07 17:15:40 -0700

    Finish new draft

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $( function() {
         $( page[0] ).trigger( 'click' )
     }
     else {
-        $( '#justification' ).trigger( 'click' )
+        $( '#introduction' ).trigger( 'click' )
     }
 
     var navstate = 1;
@@ -72,7 +72,7 @@ function build_content( data ) {
     var subnav = $( 'ul#main_subnav' );
 
     var hash = window.location.hash;
-    if ( !hash ) hash = '#justification';
+    if ( !hash ) hash = '#introduction';
     var nav = hash.split('-');
 
     var new_stuff = $( '<div></div>' );
@@ -260,7 +260,7 @@ function build_sub_list( pid, list ) {
     var subnav = $( '<ul id="SN-' + pid + '" style="display: none;" class="second_subnav listnav"></ul>' );
 
     var hash = window.location.hash;
-    if ( !hash ) hash = '#justification';
+    if ( !hash ) hash = '#introduction';
     var nav = hash.split('-');
 
     list.find( 'dt' ).each( function() {
